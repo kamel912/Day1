e3f03edb42c4ce26e6d55f6752c9c7a25bd52ac8
kamel912 <mk.25390@gmail.com>
2016-12-11 02:12:16 +0200

    Initial commit

---
+++ app/src/main/java/com/mkapps/day1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mkapps.day1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.*;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public void toto(View View){
+        Log.i("Info", "Button Tapped ");
+        Toast.makeText(this, "Hello To MK Apps", Toast.LENGTH_SHORT).show();
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
